af1fa7c37fb9f0bd65194696d592ee9dae56ee61
Maria <maria.b.kwiatkowska@gmail.com>
2019-02-06 19:27:16 +0100

    2019-02-06 19:30

--- thinkpython.py
+++ thinkpython.py
@@ -1,3 +1,10 @@
+"""
+TODO: review
+
+    * 
+
+"""
+
 import math
 
 
@@ -11,3 +18,68 @@ def hypotenuse(a, b):
 
 hyp = hypotenuse(3, 4)
 print(hyp)
+
+
+# chapter 6.4, boolean functions
+def is_between(x, y, z):
+    if(x <= y <= z):
+        return True
+    else:
+        return False
+
+ans = is_between(2, 5, 4)
+print(ans)
+
+
+# recursive palindrome checker
+def is_palindrome(word):
+    if (len(word) == 0):
+        raise ValueError("ochujałeś?")
+    if (ret_first(word) == ret_last(word)):
+        if (len(ret_middle(word)) > 1):
+            return is_palindrome(ret_middle(word))
+        else:
+            return True
+    else:
+        return False
+
+
+def ret_first(word):
+    return word[0]
+
+
+def ret_last(word):
+    return word[-1]
+
+
+def ret_middle(word):
+    return word[1:-1]
+
+
+lista = ["krok", "ara", "onomatopeja", "abba", ""]
+
+# szybki czit szit do trajkeczowania
+for each in lista:
+    try:
+        print(is_palindrome(each))
+    except ValueError as err:
+        print('wyjebalo ci to {:s}'.format(str(err)))
+    else:
+        print(':3')
+    finally:
+        print('przeiterowalem bez bolu')
+
+
+def is_power(a, b):
+    if (a % b == 0):
+        if (((a / b) % b) == 0):
+            return True
+        else:
+            return "trochę tak, ale nie"
+    else:
+        return False
+
+
+print(is_power(1024, 2))
+
+
