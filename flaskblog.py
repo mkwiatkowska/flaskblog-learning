cdc0664d622eeac01d3fb4eb0d33cb153e386919
Maria <maria.b.kwiatkowska@gmail.com>
2019-03-23 20:37:34 +0100

    szkielet strony, html, css, py, zakończony tutorial 2

--- flaskblog.py
+++ flaskblog.py
@@ -1,17 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 #zainstancjonowanie appki flaskowej
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Maria Kwiatkowska',
+        'title': 'flaskblog',
+        'content': 'testing post',
+        'date_posted': '23-03-2019'
+    },
+    {
+        'author': 'Jan Nowak',
+        'title': 'Jan Blog',
+        'content': 'Post Janka',
+        'date_posted': '03-02-2019'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
-def hello():
-    return render_template('home.html')
+def home():
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 
 if __name__ == "__main__":
