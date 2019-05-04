ed39f71a1e9512b13f0a274b166e509e1430c51e
Maria <maria.b.kwiatkowska@gmail.com>
2019-05-04 13:31:12 +0200

    end of restructuring, at least for now

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -9,3 +9,5 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'a8a1b7728c69b879f4c218afb8d49e36'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+
+from flaskblog import routes
