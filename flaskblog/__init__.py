7b0fc6a8ddee831749635ed8210b649811c5d582
Maria <maria.b.kwiatkowska@gmail.com>
2019-05-13 13:24:35 +0200

    added redirection to next parameter in url after logging in, also added mandatory login before opening account page

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -13,5 +13,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+#klasa bootstrapa
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,8 +1,8 @@
 from flaskblog.models import User, Post
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
-from flask import render_template, url_for, flash, redirect
-from flask_login import login_user, logout_user, current_user
+from flask import render_template, url_for, flash, redirect, request
+from flask_login import login_user, logout_user, current_user, login_required
 
 posts = [
     {
@@ -55,7 +55,8 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('couldnt log u in, sorry, check email and/or password', 'danger')
     return render_template('login.html', title='Login Page', form=form)
@@ -65,3 +66,9 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title='Account')
