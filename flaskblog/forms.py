2051172a375917a507a1c48bf10c2cdda817fe5a
Maria <maria.b.kwiatkowska@gmail.com>
2019-05-19 19:17:11 +0200

    added account-picture upload with automatic rezise of the uploaded pic

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,18 +1,20 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms.validators import (
+    DataRequired, Length, Email, EqualTo, ValidationError)
 from flaskblog.models import User
 
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username',
-            validators=[DataRequired(), Length(min=2, max=20)])
+    username = StringField(
+        'Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',
-            validators=[DataRequired(), EqualTo('password')])
+    confirm_password = PasswordField(
+        'Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
@@ -26,8 +28,6 @@ class RegistrationForm(FlaskForm):
             raise ValidationError('Email already exists in db')
 
 
-
-
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
@@ -36,9 +36,11 @@ class LoginForm(FlaskForm):
 
 
 class UpdateAccountForm(FlaskForm):
-    username = StringField('Username',
-            validators=[DataRequired(), Length(min=2, max=20)])
+    username = StringField(
+        'Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField(
+        'Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Update')
 
     def validate_username(self, username):
